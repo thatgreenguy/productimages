6d088b1ddc7ac053b6915e4895be17187a00a5c6
Paul Green <thatgreenguy@techie.net>
2019-06-21 08:10:46 +0000

    upgrade express validator to 6.x

--- app/routes/images.js
+++ app/routes/images.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
 const database = require('../database');
 const helpers = require('../helpers');
-const {check, query} = require('express-validator/check');
+const { check } = require('express-validator');
 
-const validate = [
-  check('product')
-  .not().isEmpty().withMessage('Product code required.')
-  .isLength({ min:1, max:25 }).withMessage('Product code must be 1-25 characters.')
-  .trim().stripLow(),
-  check('filter').trim().stripLow()
-]
+/*
+const datachecks = {
+  get: [
+    check('product')
+    .not().isEmpty().withMessage('Product code required.')
+    .isLength({ min:1, max:25 }).withMessage('Product code must be 1-25 characters.')
+    .trim().stripLow(),
+    check('filter').trim().stripLow()
+  ]
+}
+*/
 
-router.get('/', validate, async (req, res) => {
+router.get('/', [
+  check('product').not().isEmpty().withMessage('Product code required.')
+  check('product').isLength({ min:1, max:25 }).withMessage('Product should 
+be 1-25 characters.')
+], async (req, res) => {
 
     if ( !helpers.validationErrors(req, res) ) {
 
@@ -31,6 +39,32 @@ router.get('/', validate, async (req, res) => {
       } catch ( err ) {
         helpers.errorPayload( err, res );
       }      
+    }
+
+});
+
+router.delete('/:id',  async (req, res) => {
+
+    if ( !helpers.validationErrors(req, res) ) {
+
+      try {
+
+        let product = req.query.product;
+        let filters = helpers.returnArrayOfWords( 
+req.query.filter );
+
+        let payload = {};
+
+        dbData = await database.listImageUrls(product, 
+filters);
+
+        payload.rowCount = dbData.rowCount;
+        payload.rows = dbData.rows;
+        res.status(200).send( {success: payload} );
+
+      } catch ( err ) {
+        helpers.errorPayload( err, res );
+      }
     }
 
 });
